51b810ecedebc61abb7f644080d6b95424410443
Lucas Machado <lukaasmachado143@gmail.com>
2023-04-15 22:23:23 -0300

    Criando Modelos de domínio

---
+++ LanchesMAC(WEB-APP)/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace LanchesMAC_WEB_APP_.Models
+{
+    public class Categoria
+    {
+        public Guid CategoriaId { get; set; }
+        public string NomeCategoria { get; set; }
+        public string Descricao { get; set; }
+
+        public List<Item> Itens { get; set; }
+    }
+}
---
+++ LanchesMAC(WEB-APP)/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿namespace LanchesMAC_WEB_APP_.Models
+{
+    public class Item
+    {
+        public Guid ItemId { get; set; }
+        public string NomeItem { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsItemPreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        public Guid CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set;}
+
+    }
+}
