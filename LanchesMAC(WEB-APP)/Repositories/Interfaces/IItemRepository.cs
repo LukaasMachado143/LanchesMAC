4274e011c495905483068d09a051dcbf7c82f879
Lucas Machado <lukaasmachado143@gmail.com>
2023-04-16 15:49:24 -0300

    Criação e implementação do Repositório de Itens.

---
+++ LanchesMAC(WEB-APP)/Repositories/Interfaces/IItemRepository.cs
@@ -0,0 +1,11 @@
+﻿using LanchesMAC_WEB_APP_.Models;
+
+namespace LanchesMAC_WEB_APP_.Repositories.Interfaces
+{
+    public interface IItemRepository
+    {
+        IEnumerable<Item> Itens { get; }
+        IEnumerable<Item> ItensPreferidos { get; }
+        Item GetItemById(int id);
+    }
+}
---
+++ LanchesMAC(WEB-APP)/Repositories/ItemRepository.cs
@@ -0,0 +1,27 @@
+﻿using LanchesMAC_WEB_APP_.Context;
+using LanchesMAC_WEB_APP_.Models;
+using LanchesMAC_WEB_APP_.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMAC_WEB_APP_.Repositories
+{
+    public class ItemRepository : IItemRepository
+    {
+        private readonly AppDbContext _context;
+        public ItemRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Item> Itens => _context.Itens.Include(c => c.Categoria);
+
+        public IEnumerable<Item> ItensPreferidos => _context.Itens.
+                                                    Where(x => x.IsItemPreferido).
+                                                    Include(c => c.Categoria);
+
+        public Item GetItemById(int id)
+        {
+            var itemEncontrado = _context.Itens.FirstOrDefault(i => i.ItemId == id);
+            return itemEncontrado;
+        }
+    }
+}
