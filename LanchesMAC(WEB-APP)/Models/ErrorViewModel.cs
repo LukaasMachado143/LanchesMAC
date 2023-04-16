d2eb3b4d0af5c52a95c4e9e5cbdb0c970e26bc61
Lucas Machado <lukaasmachado143@gmail.com>
2023-04-15 21:35:56 -0300

    Startando o projeto !!!

---
+++ LanchesMAC(WEB-APP)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LanchesMAC_WEB_APP_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
