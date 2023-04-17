8d7cc6a456ba672eb83e0f15d1949bb264d49912
Lucas Machado <lukaasmachado143@gmail.com>
2023-04-16 21:34:31 -0300

    inicio da lógica do carrinho de compras.

---
+++ LanchesMAC(WEB-APP)/Models/CarrinhoCompra.cs
@@ -0,0 +1,31 @@
+﻿using LanchesMAC_WEB_APP_.Context;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace LanchesMAC_WEB_APP_.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+        public CarrinhoCompra(AppDbContext context) => _context = context;
+
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            var context = services.GetService<AppDbContext>();
+
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            session.SetString("CarrinhoId", carrinhoId);
+
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+        }   
+
+    }
+}
